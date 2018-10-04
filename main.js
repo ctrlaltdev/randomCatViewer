af92beb866a47836ef57a437df0768666f3140db
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-10-03 17:49:21 -0700

    switch to XHR for compatibility

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 (() => {
-  fetch('https://aws.random.cat/meow')
-    .then(r => r.json())
-    .then((r) => {
-      document.querySelector('main').style.backgroundImage = 'url(' + r.file + ')'
-    })
+  function reqListener () {
+    document.querySelector('main').style.backgroundImage = 'url(' + JSON.parse(this.response).file + ')'
+  }
+  
+  const oReq = new XMLHttpRequest()
+  oReq.addEventListener("load", reqListener)
+  oReq.open("GET", "https://aws.random.cat/meow")
+  oReq.send()
 })()
