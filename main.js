39a30800b6876e1b9392f4329ff1cf04f06c31a7
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-10-10 00:00:46 -0700

    Revert to fetch
    This reverts commit e71fcfad01b6f4092bcacbfaba9697c139d0c6ae.

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 (() => {
-  function reqListener () {
-    document.querySelector('main').style.backgroundImage = 'url(' + JSON.parse(this.response).file + ')'
-  }
-  
-  const oReq = new XMLHttpRequest()
-  oReq.addEventListener("load", reqListener)
-  oReq.open("GET", "https://aws.random.cat/meow")
-  oReq.send()
+  fetch('http://aws.random.cat/meow')
+    .then(r => r.json())
+    .then((r) => {
+      document.querySelector('main').style.backgroundImage = 'url(' + r.file + ')'
+    })
 })()
